c15cddab1b567c0f8c80b7e03bfee91a818948c5
DogukanGun <dogukangundogan5@gmail.com>
2023-08-24 19:32:14 +0000

    New bacalhau runner

--- blockchain/Filecoin/bacalhau_run.sh
+++ blockchain/Filecoin/bacalhau_run.sh
@@ -1,3 +1,3 @@
-sudo docker build -t  dogukangun/donationforuniversity:go .
-sudo docker push dogukangun/donationforuniversity:go
-bacalhau docker run --wait dogukangun/donationforuniversity:go
+sudo docker build -t  dogukangun/donationforuniversity:go_latest6 .
+sudo docker push dogukangun/donationforuniversity:go_latest6
+bacalhau docker run --wait --network=http --domain=eth.public-rpc.com -e WALLET_ADDRESS=0x6097f22127E2EF98C2cF31335Bede16D742f6890 dogukangun/donationforuniversity:go_latest6
--- execution/score/binary/README.md
+++
@@ -1,3 +0,0 @@
-# Binary
-
-Directory for output binaries
